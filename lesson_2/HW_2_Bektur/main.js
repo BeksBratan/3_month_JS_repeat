98540f8aae7fa07b01b087d1fcfc557e31d256d9
unknown <erma280191@gmail.com>
2022-07-16 16:51:15 +0600

    'beg'

---
+++ lesson_2/HW_2_Bektur/main.js
@@ -0,0 +1,61 @@
+// 1) используя setTimout и рекурсию
+// заставить блок дигаться
+// по определенному шаблону(как было показано на уроке)
+
+const block = document.querySelector('.block')
+
+let positionX = 0
+let positionY = 0
+
+
+const move = ()=>{
+    if(positionX <= 440 && positionY == 0){
+        positionX += 16
+        block.style.left = `${positionX}px`
+        setTimeout(move, 100)
+    }else if(positionX >= 440 && positionY <= 440){
+        positionY += 16
+        block.style.top = `${positionY}px`
+        setTimeout(move, 100)
+    }else if(positionY >= 440 && positionX != 0){
+        positionX -= 16
+        block.style.left = `${positionX}px`
+        setTimeout(move, 100)
+    }else if (positionX == 0 && positionY != 0){
+        positionY -=16
+        block.style.top = `${positionY}px`
+        setTimeout(move,100)
+    }
+}
+move()
+
+
+// const move = () => {
+// 	if (position <= 360 && topPosition == 0) {
+// 		position += 16;
+// 		block.style.left = `${position}px`;
+// 		setTimeout(move, 300);}
+// 	else if (position >= 360 && topPosition <= 360) {
+// 		topPosition += 16;
+// 		block.style.top = `${topPosition}px`;
+// 		setTimeout(move, 300);
+// 	} else if (topPosition >= 360 && position != 0) {
+// 		position -= 16;
+// 		block.style.left = `${position}px`;
+// 		setTimeout(move, 300);
+// 	} else if (position == 0 && topPosition != 0) {
+// 		topPosition -= 16;
+// 		block.style.top = `${topPosition}px`;
+// 		setTimeout(move, 300);
+// 	}
+// };
+// 2) создать Интервал который будет выводить
+// в консоль колтчество секунд, прошедших с
+// момента запуска программы.
+
+let num = 0
+const interval = setInterval(() =>{
+    num++
+    console.log(`С момента запуска программы прошло : ${num}sec`)
+},1000)
+
---
+++ lesson_2/main.js
@@ -0,0 +1,57 @@
+// // // console.log('2')
+// // //
+// // //
+// // //
+// // // const hi = ()=>{
+// // //     console.log('hi')
+// // // }
+// // // setTimeout(hi, 2000)
+// // // console.log('3')
+// //
+// //
+// // const interval = setInterval(()=>{
+// //     console.log('interval')
+// // }, 1200)
+// //
+// // setTimeout(()=>{
+// //     clearInterval(interval)
+// // }, 3000)
+//
+// const func = (n) =>{
+//     return function (num) {
+//         return n + num
+//     }
+// }
+//
+// const addOne = func(1)
+// console.log(addOne(10))
+//
+// const fun2 = (num)=>{
+//     const culk =() =>{
+//         console.log(1000 * num)
+//     }
+//     culk()
+// }
+// fun2(24)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
